bfedfd677fe0f8be0c450c9fddb0a6194ec1507d
Ben Zhang <ben-z@users.noreply.github.com>
2023-05-18 14:26:56 -0700

    Fix off by one issues in deserialize_cdr

--- src/rosbags/serde/serdes.py
+++ src/rosbags/serde/serdes.py
@@ -39,7 +39,7 @@ def deserialize_cdr(
     msgdef = get_msgdef(typename, typestore)
     func = msgdef.deserialize_cdr_le if little_endian else msgdef.deserialize_cdr_be
     message, pos = func(rawdata[4:], 0, msgdef.cls, typestore)
-    assert pos + 4 + 3 >= len(rawdata)
+    assert pos + 4 + 3 + 1 >= len(rawdata)
     return message
 
 
